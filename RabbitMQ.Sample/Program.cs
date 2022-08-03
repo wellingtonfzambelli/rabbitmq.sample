559785a3b938a882d4d9d95ecfb4b826159b8249
wellingtonmfernandes <wellington.m.fernandes@gmail.com>
2022-08-02 23:20:03 -0300

    add sample TTL

---
+++ RabbitMQ.Sample/Program.cs
@@ -0,0 +1,31 @@
+﻿using RabbitMQ.Client;
+using System;
+
+namespace RabbitMQ.Sample
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var queueName = "test_time_to_live";
+
+            var factory = new ConnectionFactory() { HostName = "localhost" };
+            using (var connection = factory.CreateConnection())
+            using (var channel = connection.CreateModel())
+            {
+                channel.QueueDeclare(queue: queueName, durable: false, exclusive: false, autoDelete: false, arguments: null);
+
+                var body = Encoding.UTF8.GetBytes($"Hellog World! Date/Hour: {DateTime.Now}");
+
+                var props = channel.CreateBasicProperties();
+
+                props.Expiration = "20000";
+
+                channel.BasicPublish(exchange: "", routingKey: queueName, basicProperties: props, body: body);
+            }
+
+            Console.WriteLine("Press [enter] to exit");
+            Console.ReadLine();
+        }
+    }
+}
