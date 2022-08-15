c52ddf806fa4bf4767bb990c602cf37323ddc1ef
wellingtonmfernandes <wellington.m.fernandes@gmail.com>
2022-08-15 14:16:30 -0300

    sample producer/consumer

--- RabbitMQ.Sample/Program.cs
+++ RabbitMQ.Sample/Program.cs
@@ -1,31 +1,77 @@
 ﻿using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
 using System;
+using System.Text;
 
-namespace RabbitMQ.Sample
+namespace RabbitMQ.Sample.Producer.Consumer
 {
     internal class Program
     {
+        private const string HOST_NAME = "localhost";
+        private const string QUEUE = "q.hello";
+        private const string EXCHANGE = "x.hello";
+        private const string ROUTING_KEY = "rk.hello";
+
         static void Main(string[] args)
         {
-            var queueName = "test_time_to_live";
+            Producer();
+            Consumer();
+        }
 
-            var factory = new ConnectionFactory() { HostName = "localhost" };
+        static void Producer()
+        {
+            var factory = new ConnectionFactory() { HostName = HOST_NAME};
             using (var connection = factory.CreateConnection())
             using (var channel = connection.CreateModel())
             {
-                channel.QueueDeclare(queue: queueName, durable: false, exclusive: false, autoDelete: false, arguments: null);
+                channel.QueueDeclare(queue: QUEUE,
+                                     durable: false,
+                                     exclusive: false,
+                                     autoDelete: false,
+                                     arguments: null);
 
-                var body = Encoding.UTF8.GetBytes($"Hellog World! Date/Hour: {DateTime.Now}");
+                string message = "Hello World!";
+                var body = Encoding.UTF8.GetBytes(message);
 
-                var props = channel.CreateBasicProperties();
+                channel.BasicPublish(exchange: EXCHANGE,
+                                     routingKey: ROUTING_KEY,
+                                     basicProperties: null,
+                                     body: body);
 
-                props.Expiration = "20000";
-
-                channel.BasicPublish(exchange: "", routingKey: queueName, basicProperties: props, body: body);
+                Console.WriteLine($" Sent {message}");
             }
 
-            Console.WriteLine("Press [enter] to exit");
+            Console.WriteLine(" Press [enter] to exit.");
             Console.ReadLine();
+        }
+
+        static void Consumer()
+        {
+            var factory = new ConnectionFactory() { HostName = HOST_NAME };
+            using (var connection = factory.CreateConnection())
+            using (var channel = connection.CreateModel())
+            {
+                channel.QueueDeclare(queue: QUEUE,
+                                     durable: false,
+                                     exclusive: false,
+                                     autoDelete: false,
+                                     arguments: null);
+
+                var consumer = new EventingBasicConsumer(channel);
+                consumer.Received += (model, ea) =>
+                {
+                    var body = ea.Body.ToArray();
+                    var message = Encoding.UTF8.GetString(body);
+                    Console.WriteLine($" Received: {message}");
+                };
+
+                channel.BasicConsume(queue: QUEUE,
+                                     autoAck: true,
+                                     consumer: consumer);
+
+                Console.WriteLine(" Press [enter] to exit.");
+                Console.ReadLine();
+            }
         }
     }
 }
